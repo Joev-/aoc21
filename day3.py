1a5e47e29ffb0cadb9e1f7da95c9a8762a465eff
Joseph Vaughan <joe@vghn.net>
2021-12-04 01:07:10 +0000

    Day 3 - Part 2

--- day3.py
+++ day3.py
@@ -1,5 +1,3 @@
-from functools import reduce 
-
 f = open('inputs/day3.txt')
 data = f.readlines()
 
@@ -15,3 +13,14 @@ gamma = int(''.join(r), 2)
 epsilon = gamma ^ 0xFFF
 print(f"γ: {gamma} ε: {epsilon} P1: {epsilon * gamma}")
 
+def reduce(d, condition, n = 0):
+    if len(d) == 1:
+        return d[0]
+    mp = list(map(list, zip(*d)))
+    rp = ''.join(list(map(lambda c: '1' if condition(c) else '0', mp)))
+    return reduce(list(filter(lambda v: v[n] == rp[n], d)), condition, n+1)
+
+o = reduce(data, lambda c: c.count('1') >= c.count('0'))
+c = reduce(data, lambda c: c.count('1') < c.count('0'))
+
+print(f"P2 {int(o, 2) * int(c, 2)}")
